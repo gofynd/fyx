83f623a1f05324de58bfebdf4cbfa0b223d68633
omprakash1989 <oppradhan2011@gmail.com>
2017-03-16 17:20:26 +0530

    Added constant and response handler.

---
+++ bluedart/constants.py
@@ -0,0 +1,19 @@
+"""
+File: constants
+Author: Om Prakash <omprakash@gofynd.com>
+Date: 16/03/2017
+
+It contains constant values for bluedart.
+"""
+
+BLUEDART_DEBUG_BASE_URL = 'http://netconnect.bluedart.com/Demo/ShippingAPI/'
+BLUEDART_BASE_URL = 'https://netconnect.bluedart.com/Ver1.7/ShippingAPI/'
+TEST_CREDS = {
+    "api_type": "S",
+    "licence_key": "e3ea61f7b9081f91b193ba99fb9c18e0",
+    "tracking_license_key": "e3ea61f7b9081f91b193ba99fb9c18e0",
+    "login_id": "BOM06144",
+    "version": "1.7",
+    "customer_code": "116745",
+    "debug": True
+}
