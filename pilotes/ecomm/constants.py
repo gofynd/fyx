fcb16305da15577f329eb430a5dbfb5e774f15ef
Vineet Dubey <vineetdubey@gofynd.com>
2017-03-22 19:57:06 +0530

    WIP:Added CreateAWB for Ecom

--- pilotes/ecomm/constants.py
+++ pilotes/ecomm/constants.py
@@ -8,9 +8,9 @@ This file contains the constants needed for making Ecomm API calls
 
 ECOM_PLACE_SHIPMENT = "/apiv2/manifest_awb/"
 ECOM_CANCEL_SHIPMENT = "/apiv2/cancel_awb/"
-# Ecomm
 ECOM_DEBUG_BASE_URL = "http://ecomm.prtouch.com"
 ECOM_BASE_URL = "http://api.ecomexpress.in"
+ECOM_FETCH_AWBS = "/apiv2/fetch_awb/"
 TEST_CREDS = {
     "username": 'ecomexpress',
     "password": 'K782&%#jkhas',
--- pilotes/ecomm/services.py
+++ pilotes/ecomm/services.py
@@ -8,8 +8,7 @@ It contains all the core service methods required for the delhivery.
 import json
 
 from pilotes.ecomm import EcommBase
-from constants import ECOM_PLACE_SHIPMENT, ECOM_CANCEL_SHIPMENT, ECOM_BASE_URL, ECOM_DEBUG_BASE_URL
-from helper import ECOMXMLParser
+from constants import ECOM_PLACE_SHIPMENT, ECOM_CANCEL_SHIPMENT, ECOM_BASE_URL, ECOM_DEBUG_BASE_URL, ECOM_FETCH_AWBS
 
 
 class CreateShipment(EcommBase):
@@ -28,12 +27,14 @@ class CreateShipment(EcommBase):
         TODO: Serialization to be added.
         :return:
         """
-        self.prepared_data["json_input"] = json.dumps([data])
+
         self.url = ECOM_BASE_URL + ECOM_PLACE_SHIPMENT
         if self.profile.debug:
             self.url = ECOM_DEBUG_BASE_URL + ECOM_PLACE_SHIPMENT
         self.prepared_data['username'] = self.profile.username
         self.prepared_data['password'] = self.profile.password
+
+        self.prepared_data["json_input"] = json.dumps([data])
         self.method = 'POST'
         self.logger.info("Payload received for creating package\n{}".format(self.prepared_data))
 
@@ -47,6 +48,7 @@ class CreateShipment(EcommBase):
 
 class CancelShipment(EcommBase):
     def __init__(self, user_profile):
+        self.prepared_data = dict()
         super(CancelShipment, self).__init__(user_profile)
 
     def _prepare_pre_request_data(self, awbs):
@@ -60,6 +62,8 @@ class CancelShipment(EcommBase):
             self.prepared_data["awbs"] = ",".join(awbs)
         elif isinstance(awbs, str):
             self.prepared_data["awbs"] = awbs
+        elif isinstance(awbs, int):
+            self.prepared_data["awbs"] = str(awbs)
 
         self.url = ECOM_BASE_URL + ECOM_CANCEL_SHIPMENT
         if self.profile.debug:
@@ -84,3 +88,43 @@ class CancelShipment(EcommBase):
         except:
             self.response = {}
 
+
+class CreateAWB(EcommBase):
+    # Two allowed values for AWB Type are COD and PPD
+    def __init__(self, user_profile, awb_type='COD'):
+        self.awb_type = awb_type
+        self.prepared_data = dict()
+        super(CreateAWB, self).__init__(user_profile)
+
+    def _prepare_pre_request_data(self, awbs):
+
+        """
+        Prepare data for request.
+        TODO: Serialization to be added.
+        :return:
+        """
+        self.prepared_data['username'] = self.profile.username
+        self.prepared_data['password'] = self.profile.password
+        self.prepared_data['count'] = 1
+        self.url = ECOM_BASE_URL + ECOM_FETCH_AWBS
+        if self.profile.debug:
+            self.url = ECOM_DEBUG_BASE_URL + ECOM_FETCH_AWBS
+        self.method = 'POST'
+        self.logger.info("Payload received to create AWB numbers\n{}".format(self.prepared_data))
+
+    def _prepare_response(self):
+        """
+        This method is overridden.
+        Prepare response in json.
+        :return: None
+        """
+        formatted_response = {}
+        try:
+            if self.response and self.response.ok:
+                json_response = self.response.json()
+                formatted_response = json_response
+            else:
+                formatted_response['status'] = 'False'
+            self.response = formatted_response
+        except:
+            self.response = {}
